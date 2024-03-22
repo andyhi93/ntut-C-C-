3c5aafb303bc8c1c3f37fbb536ef246f3bf4a6d7
lougan <134981472+andyhi93@users.noreply.github.com>
2024-03-22 23:44:22 +0800

    Add files via upload

---
+++ hw13.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int GCD(int a,int b){
+    a=abs(a);
+    b=abs(b);
+    if(b>a){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+void mfraction(int numerator,int denominator,char temp[100],int *z){
+    int g=GCD(numerator,denominator);
+    if(numerator%denominator!=0){
+        if(numerator>0 && denominator>0){
+            sprintf(temp,"y = %d/%dx",abs(numerator/g),abs(denominator/g));
+        }
+        else if(numerator<0 && denominator<0){
+            sprintf(temp,"y = %d/%dx",abs(numerator/g),abs(denominator/g));
+        }
+        else if(denominator<0){
+            sprintf(temp,"y = -%d/%dx",numerator/g,-denominator/g);
+        }
+        else{
+            sprintf(temp,"y = -%d/%dx",-numerator/g,denominator/g);
+        }
+    }
+    else if(numerator/denominator==0){
+        *z=1;
+        sprintf(temp,"y = ");
+    }
+    else{
+        if(numerator/denominator==1){
+            sprintf(temp,"y = x");
+        }
+        else if(numerator/denominator==-1){
+            sprintf(temp,"y = -x");
+        }
+        else{
+            sprintf(temp,"y = %dx",numerator/denominator);
+        }
+    }
+}
+
+void bfraction(int numerator,int denominator,char ans[100],int z){
+    int g=GCD(numerator,denominator);
+    if(numerator%denominator!=0){
+        if(numerator>0 && denominator>0){
+            if(z==1){
+                sprintf(ans,"%d/%d",abs(numerator/g),abs(denominator/g));
+            }
+            else{
+                sprintf(ans," + %d/%d",abs(numerator/g),abs(denominator/g));
+            }
+        }
+        else if(numerator<0 && denominator<0){
+             if(z==1){
+                sprintf(ans,"%d/%d",abs(numerator/g),abs(denominator/g));
+            }
+            else{
+                sprintf(ans," + %d/%d",abs(numerator/g),abs(denominator/g));
+            }
+        }
+        else if(denominator<0){
+            sprintf(ans," - %d/%d",numerator/g,-denominator/g);
+        }
+        else{
+            sprintf(ans," - %d/%d",-numerator/g,denominator/g);
+        }
+    }
+    else if(numerator/denominator==0){
+        sprintf(ans," ");
+    }
+    else{
+        if(numerator/denominator>0){
+             if(z==1){
+                sprintf(ans,"%d",numerator/denominator);
+            }
+            else{
+                sprintf(ans," + %d",numerator/denominator);
+            }
+        }
+        else{
+            sprintf(ans," - %d",-numerator/denominator);
+        }
+    }
+}
+
+int main(){
+    int times,x1,y1,x2,y2,z=0;
+    char ans[5][100],temp[100];
+    scanf("%d",&times);
+    for(int i=0;i<times;i++){
+        scanf(" %d %d %d %d",&x1,&y1,&x2,&y2);
+        mfraction(y1-y2,x1-x2,ans[i],&z);
+        bfraction(x2*y1-x1*y2,x2-x1,temp,z);
+        strcat(ans[i],temp);
+    }
+    for(int i=0;i<times;i++){
+        printf("%s\n",ans[i]);
+    }
+}
