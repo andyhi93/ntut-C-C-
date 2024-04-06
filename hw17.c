86e9f1f06f9223f25dda15114a00369d076ed5ff
lougan <134981472+andyhi93@users.noreply.github.com>
2024-04-06 14:15:06 +0800

    Add files via upload

--- hw17.c
+++ hw17.c
@@ -1,33 +1,85 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
-void DF(int n,int m,char ans[101][10],int data[],int dindex,int aindex,int squareNum){//n當前格子大小 m除了幾次
-    if(squareNum==0){return;}
-    if(data[dindex]==0){DF(n,m,ans,data,dindex+1,aindex,squareNum-1);}
-    if(data[dindex]==1){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                sprintf(ans[aindex++],"%d,%d",abs(squareNum-4)%2*n+i,abs(squareNum-4)%2*n+j);
-            }
-        }
+#define SWAP(x,y) {char t[100]; strcpy(t,x); strcpy(x,y); strcpy(y,t);}
+int getMinIndex(char d[101][10], int left, int right) {
+    int i=0, minIndex = left;
+    for ((i=left+1); i<right; i++) {
+        if (d[i][0]-'0'<d[minIndex][0]-'0') minIndex=i;
+        else if(d[i][0]-'0'==d[minIndex][0]-'0' &&d[i][2]-'0'<d[minIndex][2]-'0') minIndex=i;
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    return minIndex;
+}
+void sortData(char ans[101][10],int anslen){
+    for(int i=0;i<anslen;i++){
+        int index=getMinIndex(ans,i,anslen);
+        SWAP(ans[i],ans[index]);
+    }
+}
+
+void findBlack(int offsetx,int offsety,int offsetN,char ans[101][10],int *aindex){
+    for(int i=0;i<offsetN;i++){
+        for(int j=0;j<offsetN;j++){
+            sprintf(ans[(*aindex)++],"%d,%d",j+offsety,i+offsetx);
         }
     }
 }
 
+void DF(int N,char layer[100],int data[],int *dindex,char ans[101][10],int *aindex,int lenth,int sqrnum){
+    if(*dindex>lenth-1){return;}
+    if(sqrnum>3){
+        layer[strlen(layer)-1]='\0';
+        sqrnum=layer[strlen(layer)-1]+1-'0';
+        layer[strlen(layer)-1]=sqrnum+'0';
+        DF(N,layer,data,dindex,ans,aindex,lenth,sqrnum);
+    }
+    if(data[*dindex]==2){
+        layer[strlen(layer)]='0';
+        layer[strlen(layer)]='\0';
+        (*dindex)++;
+        DF(N,layer,data,dindex,ans,aindex,lenth,0);
+    }
+    else if(data[*dindex]==1){
+        int offsetx=0,offsety=0,offsetN=N;
+        for(int i=0;i<strlen(layer);i++){
+            offsetN/=2;
+            offsetx+=(layer[i]-'0')%2*offsetN;
+            offsety+=(layer[i]-'0')/2*offsetN;
+        }
+        findBlack(offsetx,offsety,offsetN,ans,aindex);
+        (*dindex)++;
+        layer[strlen(layer)-1]=sqrnum+1+'0';
+        layer[strlen(layer)]='\0';
+        DF(N,layer,data,dindex,ans,aindex,lenth,sqrnum+1);
+    }
+    else if(data[*dindex]==0){
+        layer[strlen(layer)-1]=sqrnum+1+'0';
+        (*dindex)++;
+        DF(N,layer,data,dindex,ans,aindex,lenth,sqrnum+1);
+    }
+}
+
 int main(){
-    int N;
-    char input[101],ans[101][10];
+    int N,dindex=1,aindex=0;
+    char input[101],ans[101][10],layer[100]="0";
     scanf("%s",input);
     scanf(" %d",&N);
-    int data[strlen(input)];
+    int lenth=strlen(input);
+    int data[lenth];
     if(input[0]=='0'){printf("all white");}
+    else if(input[0]=='1'){
+        findBlack(0,0,N,ans,&aindex);
+    }
     else{
-        for(int i=0;i<strlen(input);i++){
+        for(int i=0;i<lenth;i++){
             data[i]=input[i]-'0';
         }
-        DF(N/2,1,ans,data,1,N/2);
+        DF(N,layer,data,&dindex,ans,&aindex,lenth,0);
     }
+    sortData(ans,aindex);
+    for(int i=0;i<aindex;i++){
+        printf("%s\n",ans[i]);
+    }
+    return 0;
 }
