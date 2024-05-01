ebd8baf3da2fa878358c69d7fc79d394865180ed
lougan <134981472+andyhi93@users.noreply.github.com>
2024-05-01 19:42:50 +0800

    Add files via upload

--- hw22.c
+++ hw22.c
@@ -2,16 +2,20 @@
 #include <string.h>
 #include <ctype.h>
 
+int isUpperLetter(char letter){
+    return(letter>='A' && letter <='Z');
+}
+
 int Find_K(int start,char s[51],int k){
     int isBig=0,counter=1,maxLen=0,lenCount=1;
-    isBig=isupper(s[start]);
+    isBig=isUpperLetter(s[start]);
     //printf("%c",s[start]);
     for(int i=start+1;i<strlen(s);i++){
         if(counter==k){
             isBig=isBig==1?0:1;
             counter=0;
         }
-        if(isupper(s[i])==isBig){
+        if(isUpperLetter(s[i])==isBig){
             lenCount++;
             counter++;
             //printf("%c",s[i]);
