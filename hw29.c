555a553987a7cc78f71fc24ef82e56e853dd3097
lougan <134981472+andyhi93@users.noreply.github.com>
2024-05-22 10:51:57 +0800

    Add files via upload

---
+++ hw29.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+typedef struct node_s {
+    int data;
+    struct node_s *next;
+}node_t;
+typedef node_t *nodep_t;
+
+nodep_t create(int data) {
+    nodep_t newNode = malloc(sizeof(node_t));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+void push(nodep_t *p, int data) {
+    nodep_t newNode = create(data);
+    newNode->next = *p;
+    *p = newNode;
+}
+void pop(nodep_t *p) {
+    nodep_t current;
+    if ((*p)==NULL)
+        printf("Stack is empty\n");
+    else {
+        printf("%d\n", (*p)->data);
+        current = (*p);
+        (*p) = (*p)->next;
+        free(current);
+    }
+}
+void top(nodep_t *p){
+    if((*p)==NULL)printf("Stack is empty\n");
+    else{
+        printf("%d\n", (*p)->data);
+    }
+}
+void empty(nodep_t *p){
+    if((*p)==NULL)printf("Stack is empty\n");
+    else{
+        nodep_t current;
+        while((*p)!=NULL){
+            current = (*p);
+            (*p) = (*p)->next;
+            free(current);
+        }
+    }
+}
+void print(nodep_t *p){
+    nodep_t current;
+    if((*p)==NULL)printf("Stack is empty\n");
+    else{
+        current = (*p);
+        while(current!=NULL){
+            printf("%d\n", current->data);
+            current = current->next;
+        }
+    }
+}
+
+int main(){
+    int N;
+    nodep_t p = NULL;
+    scanf("%d",&N);
+    for(int i=0; i<N; i++){
+        int d;
+        char cmd[10];
+        scanf(" %s", cmd);
+        if(strcmp(cmd, "push")==0){
+            scanf(" %d", &d);
+            push(&p, d);
+        }
+        else if(strcmp(cmd, "pop")==0){
+            pop(&p);
+        }
+        else if(strcmp(cmd, "top")==0){
+            top(&p);
+        }
+        else if(strcmp(cmd, "empty")==0){
+            empty(&p);
+        }
+        else if(strcmp(cmd, "print")==0){
+            print(&p);
+        }
+    }
+}
